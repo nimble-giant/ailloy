517bd3a0d286b0aaab8f80b25ae973eb501c08c5
Kris Coleman <kriscodeman@gmail.com>
2026-02-14 00:34:32 -0500

    build: integrate build info variables and initialize commands
    Signed-off-by: Kris Coleman <kriscodeman@gmail.com>

--- cmd/ailloy/main.go
+++ cmd/ailloy/main.go
@@ -4,6 +4,13 @@ import (
 	"github.com/nimble-giant/ailloy/internal/commands"
 )
 
+var (
+	version = "dev"
+	commit  = "unknown"
+	date    = "unknown"
+)
+
 func main() {
+	commands.SetVersionInfo(version, commit, date)
 	commands.Execute()
 }
